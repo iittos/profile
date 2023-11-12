76a7a9b091fd33e4687e8fdca0aa1cd85a7d3461
ittos <116670376+iittos@users.noreply.github.com>
2023-11-11 18:14:39 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 // Function to handle the fade-in effect
 function fadeInOnScroll() {
-    const fadeElements = document.querySelectorAll(".fade");
+  const fadeElements = document.querySelectorAll(".fade");
 
-    // Check if elements are in viewport and fade them in
-    fadeElements.forEach((element) => {
-      const elementTop = element.getBoundingClientRect().top;
-      const elementBottom = element.getBoundingClientRect().bottom;
+  // Check if elements are in viewport and fade them in
+  fadeElements.forEach((element) => {
+    const elementTop = element.getBoundingClientRect().top;
+    const elementBottom = element.getBoundingClientRect().bottom;
 
-      // Adjust this value as needed based on when you want the fade effect to start
-      const triggerPoint = window.innerHeight - 100;
+    // Adjust this value as needed based on when you want the fade effect to start
+    const triggerPoint = window.innerHeight - 100;
 
-      if (elementTop < triggerPoint && elementBottom > 0) {
-        element.style.opacity = "1";
-      }
-    });
-  }
+    if (elementTop < triggerPoint && elementBottom > 0) {
+      element.style.opacity = "1";
+    }
+  });
+}
 
-  // Event listener for scroll event
-  window.addEventListener("scroll", fadeInOnScroll);
+// Event listener for scroll event
+window.addEventListener("scroll", fadeInOnScroll);
